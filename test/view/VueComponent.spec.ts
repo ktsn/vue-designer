3df768a6fcc7252ea1356dd04bdbb4f3342d904e
ktsn <ktsn55@gmail.com>
2018-01-14 03:32:22 +0900

    simple test for vue component component

---
+++ test/view/VueComponent.spec.ts
@@ -0,0 +1,94 @@
+import { mount, Wrapper } from '@vue/test-utils'
+import VueComponent from '@/view/components/VueComponent.vue'
+import { Template, Element, ExpressionNode } from 'parser/template'
+import { Prop } from 'parser/script'
+import { Data } from 'ws'
+
+jest.mock('../../src/view/mixins/shadow-dom', () => {
+  return {}
+})
+
+describe('VueComponent', () => {
+  it('should render template', () => {
+    const template = createTemplate([
+      h('p', { title: 'Hello' }, ['Hello World!'])
+    ])
+
+    const wrapper = render(template)
+    const p = wrapper.find('p')
+    expect(p.text()).toBe('Hello World!')
+    expect(p.attributes()!.title).toBe('Hello')
+  })
+})
+
+function render(
+  template: Template,
+  props: Prop[] = [],
+  data: Data[] = []
+): Wrapper<VueComponent> {
+  return mount(VueComponent, {
+    propsData: {
+      template,
+      props,
+      data,
+      styles: []
+    }
+  })
+}
+
+function createTemplate(
+  children: (Element | ExpressionNode | string)[]
+): Template {
+  return {
+    type: 'Template',
+    attributes: [],
+    children: children.map(c => {
+      return typeof c === 'string'
+        ? {
+            type: 'TextNode' as 'TextNode',
+            path: [],
+            text: c
+          }
+        : c
+    })
+  }
+}
+
+function h(
+  tag: string,
+  attrs: Record<string, string | null>,
+  children: (Element | ExpressionNode | string)[]
+): Element {
+  const attributes = Object.keys(attrs).map((key, i) => {
+    return {
+      type: 'Attribute' as 'Attribute',
+      index: i,
+      name: key,
+      value: attrs[key]
+    }
+  })
+
+  return {
+    type: 'Element',
+    path: [],
+    name: tag,
+    attributes,
+    children: children.map(c => {
+      return typeof c === 'string'
+        ? {
+            type: 'TextNode' as 'TextNode',
+            path: [],
+            text: c
+          }
+        : c
+    })
+  }
+}
+
+function exp(expression: string): ExpressionNode {
+  return {
+    type: 'ExpressionNode',
+    path: [],
+    expression
+  }
+}

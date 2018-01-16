8d4c4b6b227a2cac0c246f0edbfa5be3a7f55110
ktsn <ktsn55@gmail.com>
2018-01-17 01:19:09 +0900

    relayout information pane

--- src/view/components/Information.vue
+++ src/view/components/Information.vue
@@ -1,31 +1,39 @@
 <template>
-  <div class="information">
-    <section class="information-group">
-      <h2 class="information-title">Props</h2>
-      <ul class="information-list">
-        <li v-for="prop in props" :key="prop.name" class="information-list-item">
-          <strong class="information-label">{{ prop.name }}</strong>
-          <span class="information-text">
-            <span v-if="prop.default === undefined" class="information-placeholder">
-              {{ prop.type }}
+  <div class="information" :class="{ open: open }">
+    <div class="information-body">
+      <section class="information-group">
+        <h2 class="information-title">Props</h2>
+        <ul class="information-list">
+          <li v-for="prop in props" :key="prop.name" class="information-list-item">
+            <strong class="information-label">{{ prop.name }}</strong>
+            <span class="information-text">
+              <span v-if="prop.default === undefined" class="information-placeholder">
+                {{ prop.type }}
+              </span>
+              <span v-else>{{ prop.default }}</span>
             </span>
-            <span v-else>{{ prop.default }}</span>
-          </span>
-        </li>
-      </ul>
-    </section>
+          </li>
+        </ul>
+      </section>
 
-    <section class="information-group">
-      <h2 class="information-title">Data</h2>
-      <ul class="information-list">
-        <li v-for="d in data" :key="d.name" class="information-list-item">
-          <strong class="information-label">{{ d.name }}</strong>
-          <span class="information-text">
-            {{ d.default }}
-          </span>
-        </li>
-      </ul>
-    </section>
+      <section class="information-group">
+        <h2 class="information-title">Data</h2>
+        <ul class="information-list">
+          <li v-for="d in data" :key="d.name" class="information-list-item">
+            <strong class="information-label">{{ d.name }}</strong>
+            <span class="information-text">
+              {{ d.default }}
+            </span>
+          </li>
+        </ul>
+      </section>
+    </div>
+
+    <button
+      class="information-toggle"
+      type="button"
+      @click="open = !open"
+    >T</button>
   </div>
 </template>
 
@@ -45,19 +53,35 @@ export default Vue.extend({
       type: Array as { (): Data[] },
       required: true
     }
+  },
+
+  data() {
+    return {
+      open: false
+    }
   }
 })
 </script>
 
 <style lang="scss" scoped>
 .information {
-  display: flex;
-  position: absolute;
-  left: 0;
+  position: fixed;
   right: 0;
+  top: 0;
   bottom: 0;
-  height: 250px;
+  width: 250px;
   background-color: #349c47;
+  transform: translateX(100%);
+  transition: transform 400ms cubic-bezier(0.19, 1, 0.22, 1);
+
+  &.open {
+    transform: translateX(0);
+  }
+
+  &-body {
+    overflow-y: auto;
+    max-height: 100%;
+  }
 
   &-group {
     flex: 1 1 auto;
@@ -83,6 +107,14 @@ export default Vue.extend({
 
   &-text {
     margin-left: 0.5em;
+  }
+
+  &-toggle {
+    position: absolute;
+    right: 100%;
+    bottom: 0;
+    border-width: 0;
+    background: none;
   }
 }
 </style>

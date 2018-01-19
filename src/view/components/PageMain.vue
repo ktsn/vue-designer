fc53051c6c7b9d85a33f87663c2f38fac190ef58
ktsn <ktsn55@gmail.com>
2018-01-19 14:34:08 +0900

    move the logic for toggling the pane to MainPage

--- src/view/components/PageMain.vue
+++ src/view/components/PageMain.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <Renderer :template="template" :styles="styles" :props="props" :data="data" />
-    <div class="information-pane">
+    <div class="information-pane" :class="{ open: openPane }">
       <ScopeInformation :props="props" :data="data" />
+
+      <button
+        class="information-pane-toggle"
+        type="button"
+        @click="openPane = !openPane"
+      >T</button>
     </div>
   </div>
 </template>
@@ -29,7 +35,8 @@ export default Vue.extend({
       template: undefined as Template | undefined,
       props: [] as Prop[],
       data: [] as Data[],
-      styles: [] as string[]
+      styles: [] as string[],
+      openPane: false
     }
   },
 
@@ -46,3 +53,29 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.information-pane {
+  position: fixed;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 330px;
+  background-color: #f5f5f5;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+  transform: translateX(100%);
+  transition: transform 400ms cubic-bezier(0.19, 1, 0.22, 1);
+
+  &.open {
+    transform: translateX(0);
+  }
+
+  &-toggle {
+    position: absolute;
+    right: 100%;
+    bottom: 0;
+    border-width: 0;
+    background: none;
+  }
+}
+</style>
--- src/view/components/ScopeInformation.vue
+++ src/view/components/ScopeInformation.vue
@@ -1,39 +1,31 @@
 <template>
-  <div class="information" :class="{ open: open }">
-    <div class="information-body">
-      <section class="information-group">
-        <h2 class="information-title">Props</h2>
-        <ul class="information-list">
-          <li v-for="prop in props" :key="prop.name" class="information-list-item">
-            <strong class="information-label">{{ prop.name }}</strong>
-            <span class="information-text">
-              <span v-if="prop.default === undefined" class="information-placeholder">
-                {{ prop.type }}
-              </span>
-              <span v-else>{{ prop.default }}</span>
+  <div class="information">
+    <section class="information-group">
+      <h2 class="information-title">Props</h2>
+      <ul class="information-list">
+        <li v-for="prop in props" :key="prop.name" class="information-list-item">
+          <strong class="information-label">{{ prop.name }}</strong>
+          <span class="information-text">
+            <span v-if="prop.default === undefined" class="information-placeholder">
+              {{ prop.type }}
             </span>
-          </li>
-        </ul>
-      </section>
+            <span v-else>{{ prop.default }}</span>
+          </span>
+        </li>
+      </ul>
+    </section>
 
-      <section class="information-group">
-        <h2 class="information-title">Data</h2>
-        <ul class="information-list">
-          <li v-for="d in data" :key="d.name" class="information-list-item">
-            <strong class="information-label">{{ d.name }}</strong>
-            <span class="information-text">
-              {{ d.default }}
-            </span>
-          </li>
-        </ul>
+    <section class="information-group">
+      <h2 class="information-title">Data</h2>
+      <ul class="information-list">
+        <li v-for="d in data" :key="d.name" class="information-list-item">
+          <strong class="information-label">{{ d.name }}</strong>
+          <span class="information-text">
+            {{ d.default }}
+          </span>
+        </li>
+      </ul>
       </section>
-    </div>
-
-    <button
-      class="information-toggle"
-      type="button"
-      @click="open = !open"
-    >T</button>
   </div>
 </template>
 
@@ -53,37 +45,15 @@ export default Vue.extend({
       type: Array as { (): Data[] },
       required: true
     }
-  },
-
-  data() {
-    return {
-      open: false
-    }
   }
 })
 </script>
 
 <style lang="scss" scoped>
 .information {
-  position: fixed;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  width: 330px;
-  background-color: #f5f5f5;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
-  transform: translateX(100%);
-  transition: transform 400ms cubic-bezier(0.19, 1, 0.22, 1);
-
-  &.open {
-    transform: translateX(0);
-  }
-
-  &-body {
-    overflow-y: auto;
-    max-height: 100%;
-    padding: 13px 15px;
-  }
+  overflow-y: auto;
+  max-height: 100%;
+  padding: 13px 15px;
 
   &-group {
     &:not(:first-child) {
@@ -111,14 +81,6 @@ export default Vue.extend({
 
   &-text {
     margin-left: 0.5em;
-  }
-
-  &-toggle {
-    position: absolute;
-    right: 100%;
-    bottom: 0;
-    border-width: 0;
-    background: none;
   }
 }
 </style>

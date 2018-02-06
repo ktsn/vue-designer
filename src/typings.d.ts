4b0b5ac9fbe84c023acf82052c86b0f751c994af
ktsn <ktsn55@gmail.com>
2018-02-06 13:34:51 +0900

    use postcss parser for css parsing

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,4 +3,9 @@ declare module '*.vue' {
   export default Vue
 }
 
+declare module 'postcss-safe-parser' {
+  import { parse } from 'postcss'
+  export = parse
+}
+
 declare module 'vue-template-compiler'

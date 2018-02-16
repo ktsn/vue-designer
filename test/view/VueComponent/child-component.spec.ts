10313ccc0f7c5b0afdcacc183138e1623cc66249
ktsn <ktsn55@gmail.com>
2018-02-17 00:07:47 +0900

    add child component rendering test

--- test/view/VueComponent/child-component.spec.ts
+++ test/view/VueComponent/child-component.spec.ts
@@ -1,4 +1,10 @@
-import { createTemplate, h, render } from '../../parser/template-helpers'
+import {
+  createTemplate,
+  h,
+  render,
+  exp,
+  a
+} from '../../parser/template-helpers'
 
 describe('VueComponent child components', () => {
   it('should render child components in the store', () => {
@@ -33,5 +39,62 @@ describe('VueComponent child components', () => {
 
     expect(wrapper.find('p').text()).toBe('Normal element')
     expect(wrapper.find('button').text()).toBe('Component button')
+  })
+
+  it("should use child component's scope", () => {
+    // prettier-ignore
+    const template = createTemplate([
+      h('Child', [], [])
+    ])
+
+    const components = {
+      'file:///Child.vue': {
+        // prettier-ignore
+        template: createTemplate([
+          h('div', [], [
+            h('div', [a('id', 'first')], [
+              exp('parentValue')
+            ]),
+            h('div', [a('id', 'second')], [
+              exp('bothValue')
+            ])
+          ])
+        ]),
+        data: [
+          {
+            name: 'bothValue',
+            default: 'should render child value'
+          }
+        ]
+      }
+    }
+
+    const wrapper = render(
+      template,
+      [],
+      [
+        {
+          name: 'parentValue',
+          default: 'should not be rendered in child'
+        },
+        {
+          name: 'bothValue',
+          default: 'should not be rendered in child'
+        }
+      ],
+      [
+        {
+          name: 'Child',
+          uri: 'file:///Child.vue'
+        }
+      ],
+      components
+    )
+
+    // should not resolved since child component does not have the value
+    expect(wrapper.find('#first').text()).toBe('{{ parentValue }}')
+
+    // should resolved as child component value
+    expect(wrapper.find('#second').text()).toBe('should render child value')
   })
 })

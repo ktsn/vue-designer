750f24eaa8631de45781a0008b312fc3932df2fc
ktsn <ktsn55@gmail.com>
2018-03-11 22:39:08 +0900

    feat: add style value component with basic features

---
+++ src/view/components/StyleValue.vue
@@ -0,0 +1,71 @@
+<template>
+  <button
+    v-if="!editing"
+    class="style-value"
+    @click="startEdit"
+    @focus="startEdit"
+  >{{ value }}</button>
+  <div
+    v-else
+    class="style-value editing"
+    contenteditable="true"
+    ref="input"
+    @input="input"
+    @blur="endEdit"
+  >{{ value }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'StyleValue',
+
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      editing: false
+    }
+  },
+
+  methods: {
+    startEdit() {
+      this.editing = true
+      this.$nextTick(() => {
+        ;(this.$refs.input as HTMLDivElement).focus()
+      })
+    },
+
+    endEdit() {
+      this.editing = false
+    },
+
+    input(event: Event) {
+      const el = event.target as HTMLDivElement
+      this.$emit('input', el.textContent)
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.style-value {
+  display: inline;
+  padding: 0;
+  border-width: 0;
+  background: none;
+  font-family: inherit;
+  font-size: inherit;
+}
+
+.style-value.editing {
+  margin: -1px;
+  border: 1px solid #aaa;
+}
+</style>
---
+++ test/view/StyleValue/basic.spec.ts
@@ -0,0 +1,57 @@
+import StyleValue from '@/view/components/StyleValue.vue'
+import { mount } from '@vue/test-utils'
+
+describe('StyleValue basic', () => {
+  it('should render', () => {
+    const wrapper = mount(StyleValue, {
+      propsData: {
+        value: '20px'
+      }
+    })
+    expect(wrapper.attributes()!.contenteditable).not.toBe('true')
+    expect(wrapper.text()).toBe('20px')
+  })
+
+  it('should make editable when clicked', () => {
+    const wrapper = mount(StyleValue, {
+      propsData: {
+        value: '20px'
+      }
+    })
+    wrapper.trigger('click')
+    expect(wrapper.attributes()!.contenteditable).toBe('true')
+
+    const el = wrapper.find('[contenteditable]')
+    el.element.textContent = '22px'
+    el.trigger('input')
+    expect(wrapper.emitted('input')[0]).toEqual(['22px'])
+  })
+
+  it('should make editable when focused', () => {
+    const wrapper = mount(StyleValue, {
+      propsData: {
+        value: '20px'
+      }
+    })
+    wrapper.trigger('focus')
+    expect(wrapper.attributes()!.contenteditable).toBe('true')
+
+    const el = wrapper.find('[contenteditable]')
+    el.element.textContent = '22px'
+    el.trigger('input')
+    expect(wrapper.emitted('input')[0]).toEqual(['22px'])
+  })
+
+  it('should cancel editing when blured', () => {
+    const wrapper = mount(StyleValue, {
+      propsData: {
+        value: '20px'
+      }
+    })
+    wrapper.trigger('click')
+    expect(wrapper.attributes()!.contenteditable).toBe('true')
+
+    wrapper.trigger('blur')
+    expect(wrapper.attributes()!.contenteditable).not.toBe('true')
+  })
+})

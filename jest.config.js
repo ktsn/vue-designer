6066874d70064a89270b8b094357842cf27b77d4
ktsn <ktsn55@gmail.com>
2018-08-16 22:53:09 +0900

    chore: add @vue/cli-plugin-unit-jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,31 @@
 module.exports = {
   transform: {
-    '^.+\\.[jt]s$': 'ts-jest',
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.[jt]sx?$': 'ts-jest',
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub'
   },
+
   transformIgnorePatterns: ['node_modules/(?!(sinai/lib)/)'],
   setupFiles: ['<rootDir>/test/setup.ts'],
-  testRegex: '/test/.+\\.spec\\.(js|ts)$',
+  testRegex: '/test/.+\\.spec\\.(js|ts)x?$',
+
   moduleNameMapper: {
     '^@/(.+)$': '<rootDir>/src/$1',
     '^vue$': 'vue/dist/vue.runtime.common.js'
   },
-  moduleFileExtensions: ['ts', 'js', 'json', 'vue'],
-  collectCoverageFrom: ['src/**/*.{ts,vue}'],
+
+  moduleFileExtensions: ['ts', 'js', 'json', 'vue', 'jsx', 'tsx'],
+
+  collectCoverageFrom: ['src/**/*.{ts,tsx,vue}'],
+
   globals: {
     'ts-jest': {
       tsConfigFile: 'tsconfig.test.json'
     }
-  }
+  },
+
+  snapshotSerializers: ['jest-serializer-vue'],
+
+  testURL: 'http://localhost/'
 }

4307908d565d7b17ae843574bbb2643e84969e11
ktsn <ktsn55@gmail.com>
2018-08-17 23:32:54 +0900

    chore: bump deps

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import { config } from '@vue/test-utils'
 import { install } from 'sinai'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
+
+config.logModifiedComponents = false
 
 Vue.use(install)

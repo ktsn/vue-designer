1ce4287ac26cdc7a33b4b340ceb269cdce2037dd
ktsn <ktsn@hey.com>
2023-12-16 22:56:21 +0900

    chore: fix style value test

--- tests/view/StyleValue/basic.spec.ts
+++ tests/view/StyleValue/basic.spec.ts
@@ -1,193 +1,213 @@
 import StyleValue from '@/view/components/StyleValue.vue'
-import { mount } from '@vue/test-utils'
+import Vue, { nextTick } from 'vue'
+
+function mount(Component: typeof Vue, propsData?: Record<string, any>) {
+  return new Component({
+    propsData,
+  }).$mount()
+}
 
 describe('StyleValue basic', () => {
   it('should render', () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: '20px',
-      },
+    const vm = mount(StyleValue, {
+      value: '20px',
     })
-    expect(wrapper.attributes()!.contenteditable).not.toBe('true')
-    expect(wrapper.text()).toBe('20px')
+    const el = vm.$el as HTMLElement
+    expect(el.getAttribute('contenteditable')).not.toBe('true')
+    expect(el.textContent?.trim()).toBe('20px')
   })
 
   it('should make editable when clicked', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: '20px',
-      },
+    const vm = mount(StyleValue, {
+      value: '20px',
     })
-    wrapper.trigger('click')
-    await wrapper.vm.$nextTick()
+    vm.$el.dispatchEvent(new MouseEvent('click'))
+    await nextTick()
 
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
-    expect(wrapper.text()).toBe('20px')
+    const el = vm.$el as HTMLElement
+    expect(el.getAttribute('contenteditable')).toBe('true')
+    expect(el.textContent).toBe('20px')
 
-    const el = wrapper.find('[contenteditable]')
-    el.element.textContent = '22px'
-    el.trigger('input')
-    expect(wrapper.emitted('input')[0]).toEqual(['22px'])
+    const inputListener = jest.fn()
+    vm.$once('input', inputListener)
+
+    el.textContent = '22px'
+    el.dispatchEvent(new KeyboardEvent('input'))
+    expect(inputListener).toHaveBeenCalledWith('22px')
   })
 
   it('should make editable when focused', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: '20px',
-      },
+    const vm = mount(StyleValue, {
+      value: '20px',
     })
-    wrapper.trigger('focus')
-    await wrapper.vm.$nextTick()
+    vm.$el.dispatchEvent(new Event('focus'))
+    await nextTick()
 
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
-    expect(wrapper.text()).toBe('20px')
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
+    expect(vm.$el.textContent).toBe('20px')
 
-    const el = wrapper.find('[contenteditable]')
-    el.element.textContent = '22px'
-    el.trigger('input')
-    expect(wrapper.emitted('input')[0]).toEqual(['22px'])
+    const inputListener = jest.fn()
+    vm.$once('input', inputListener)
+
+    vm.$el.textContent = '22px'
+    vm.$el.dispatchEvent(new KeyboardEvent('input'))
+    expect(inputListener).toHaveBeenCalledWith('22px')
   })
 
   it('should notify starting input', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: '20px',
-      },
+    const vm = mount(StyleValue, {
+      value: '20px',
     })
-    wrapper.trigger('click')
-    await wrapper.vm.$nextTick()
-    expect(wrapper.emitted('input-start').length).toBe(1)
+
+    const listener = jest.fn()
+    vm.$on('input-start', listener)
+
+    vm.$el.dispatchEvent(new MouseEvent('click'))
+    await nextTick()
+    expect(listener).toHaveBeenCalledTimes(1)
   })
 
   it('should end editing when blured', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: '20px',
-      },
+    const vm = mount(StyleValue, {
+      value: '20px',
     })
-    wrapper.trigger('click')
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
+    vm.$el.dispatchEvent(new MouseEvent('click'))
+    await nextTick()
 
-    await wrapper.vm.$nextTick()
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
 
-    wrapper.trigger('blur')
-    expect(wrapper.attributes()!.contenteditable).not.toBe('true')
-    expect(wrapper.emitted('input-end')[0]).toEqual([
-      '20px',
-      { reason: 'blur', shiftKey: false },
-    ])
+    const listener = jest.fn()
+    vm.$on('input-end', listener)
+
+    vm.$el.dispatchEvent(new Event('blur'))
+    await nextTick()
+
+    expect(vm.$el.getAttribute('contenteditable')).not.toBe('true')
+    expect(listener).toHaveBeenCalledWith('20px', {
+      reason: 'blur',
+      shiftKey: false,
+    })
   })
 
   it('should end editing when pressed enter key', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: '20px',
-      },
+    const vm = mount(StyleValue, {
+      value: '20px',
     })
-    wrapper.trigger('click')
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
+    vm.$el.dispatchEvent(new MouseEvent('click'))
+    await nextTick()
 
-    await wrapper.vm.$nextTick()
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
 
-    wrapper.trigger('keydown', {
-      key: 'Enter',
+    const listener = jest.fn()
+    vm.$once('input-end', listener)
+
+    vm.$el.dispatchEvent(
+      new KeyboardEvent('keydown', {
+        key: 'Enter',
+      })
+    )
+
+    await nextTick()
+
+    expect(vm.$el.getAttribute('contenteditable')).not.toBe('true')
+    expect(listener).toHaveBeenCalledWith('20px', {
+      reason: 'enter',
+      shiftKey: false,
     })
-    expect(wrapper.attributes()!.contenteditable).not.toBe('true')
-    expect(wrapper.emitted('input-end')[0]).toEqual([
-      '20px',
-      { reason: 'enter', shiftKey: false },
-    ])
   })
 
   it('should end editing when pressed tab key', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: '20px',
-      },
+    const vm = mount(StyleValue, {
+      value: '20px',
     })
-    wrapper.trigger('click')
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
+    vm.$el.dispatchEvent(new MouseEvent('click'))
+    await nextTick()
 
-    await wrapper.vm.$nextTick()
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
 
-    wrapper.trigger('keydown', {
-      key: 'Tab',
+    const listener = jest.fn()
+    vm.$once('input-end', listener)
+
+    vm.$el.dispatchEvent(
+      new KeyboardEvent('keydown', {
+        key: 'Tab',
+      })
+    )
+
+    await nextTick()
+
+    expect(vm.$el.getAttribute('contenteditable')).not.toBe('true')
+    expect(listener).toHaveBeenCalledWith('20px', {
+      reason: 'tab',
+      shiftKey: false,
     })
-    expect(wrapper.attributes()!.contenteditable).not.toBe('true')
-    expect(wrapper.emitted('input-end')[0]).toEqual([
-      '20px',
-      { reason: 'tab', shiftKey: false },
-    ])
   })
 
   it('should include shift key state when end editing', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: '20px',
-      },
+    const vm = mount(StyleValue, {
+      value: '20px',
     })
-    wrapper.trigger('click')
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
+    vm.$el.dispatchEvent(new MouseEvent('click'))
+    await nextTick()
 
-    await wrapper.vm.$nextTick()
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
 
-    wrapper.trigger('keydown', {
-      key: 'Tab',
+    const listener = jest.fn()
+    vm.$once('input-end', listener)
+
+    vm.$el.dispatchEvent(
+      new KeyboardEvent('keydown', {
+        key: 'Tab',
+        shiftKey: true,
+      })
+    )
+
+    await nextTick()
+
+    expect(vm.$el.getAttribute('contenteditable')).not.toBe('true')
+    expect(listener).toHaveBeenCalledWith('20px', {
+      reason: 'tab',
       shiftKey: true,
     })
-    expect(wrapper.attributes()!.contenteditable).not.toBe('true')
-    expect(wrapper.emitted('input-end')[0]).toEqual([
-      '20px',
-      { reason: 'tab', shiftKey: true },
-    ])
   })
 
   it('should update editing content when prop is updated', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: 'red',
-      },
+    const vm = mount(StyleValue, {
+      value: 'red',
     })
-    wrapper.trigger('click')
-    await wrapper.vm.$nextTick()
+    vm.$el.dispatchEvent(new MouseEvent('click'))
+    await nextTick()
 
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
-    expect(wrapper.text()).toBe('red')
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
+    expect(vm.$el.textContent?.trim()).toBe('red')
 
-    wrapper.setProps({
-      value: 'blue',
-    })
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
-    expect(wrapper.text()).toBe('blue')
+    vm.$props.value = 'blue'
+    await nextTick()
+
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
+    expect(vm.$el.textContent?.trim()).toBe('blue')
   })
 
   it('should be editable when autoFocus is specified', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: 'red',
-        autoFocus: true,
-      },
-    })
-
-    await wrapper.vm.$nextTick()
-
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
-  })
-
-  it('should focus on input field by changing autoFocus prop', async () => {
-    const wrapper = mount(StyleValue, {
-      propsData: {
-        value: 'red',
-        autoFocus: true,
-      },
-    })
-
-    wrapper.setProps({
+    const vm = mount(StyleValue, {
+      value: 'red',
       autoFocus: true,
     })
 
-    await wrapper.vm.$nextTick()
+    await nextTick()
 
-    expect(wrapper.attributes()!.contenteditable).toBe('true')
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
+  })
+
+  it('should focus on input field by changing autoFocus prop', async () => {
+    const vm = mount(StyleValue, {
+      value: 'red',
+      autoFocus: true,
+    })
+
+    vm.$props.autoFocus = true
+    await nextTick()
+
+    expect(vm.$el.getAttribute('contenteditable')).toBe('true')
   })
 })
